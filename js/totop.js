f1babe384d9ea0c834fa6c2a777896d975bd12d6
xinayu <695601924@qq.com>
2018-07-05 17:24:08 +0800

    Site updated: 2018-07-05 17:24:02

--- js/totop.js
+++ js/totop.js
@@ -20,9 +20,11 @@ $('copy').addClass('btn-copy').each(function(index) {
     //为当前元素下的第一个代码段添加id
     $(this).parent(':first').nextAll('figure:first').find('pre').attr("id","pre"+(index+1));
     //添加映射关系
-    $(this).attr('data-clipboard-target',"#pre"+(index+1));
+    $(this).attr({
+        'data-clipboard-target':"#pre"+(index+1),
+        'title':'点击复制'
+    });
 });
-
 let clipboard = new Clipboard('.btn-copy');
 
 
